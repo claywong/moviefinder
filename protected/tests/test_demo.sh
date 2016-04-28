a8c88484bc5c312eda8d3b1be794b9bfc90cb6c6
pinguo-wangzhong <wangzhong@camera360.com>
2016-04-28 17:22:03 +0800

    modify

--- protected/tests/test_demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-APPLICATION_ENV='newdev'
-MODULE_NAME='demo'
-IDC_NUM=1
-IDC_ID=0
-
-export APPLICATION_ENV MODULE_NAME IDC_NUM IDC_ID
-phpunit --colors --coverage-html ~/data/coverage/"$MODULE_NAME" --testsuite "$MODULE_NAME" "$1"
--- protected/web/index.php
+++ protected/web/index.php
@@ -7,9 +7,9 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require(__DIR__ . '/../../vendor/autoload.php');
-require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../protected/config/web.php');
+$config = require(__DIR__ . '/../config/web.php');
 
 (new yii\web\Application($config))->run();
