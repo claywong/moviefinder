0bf93f9852a2164ed9099611dea4c663c254333b
pinguo-wangzhong <wangzhong@camera360.com>
2016-04-29 09:15:17 +0800

    ignore runtime

--- public/index.php
+++ public/index.php
@@ -1,15 +1,11 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
+require(__DIR__ . '/../protected/config/mode.php');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../protected/config/web.php');
+$config = require(__DIR__ . '/../protected/config/' . strtolower(APPLICATION_ENV) . '/main.php');
 
 (new yii\web\Application($config))->run();
